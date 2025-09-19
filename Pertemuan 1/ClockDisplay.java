e7e160d80d8c1d805425c25e69e1e9a034ede3b9
Abdullah Sultan Barizy <abdullahsultanbarizy@gmail.com>
2025-09-19 18:33:01 +0700

    Update ClockDisplay.java

--- Pertemuan 1/ClockDisplay.java
+++ Pertemuan 1/ClockDisplay.java
@@ -4,21 +4,21 @@ public class ClockDisplay{
     private NumberDisplay seconds;
     private String displayString;
     
-    public ClockDisplay(){
+    public ClockDisplay(){ // Konstruktor untuk ngambil info di kelas NumberDisplay
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
         seconds = new NumberDisplay(60);
         updateDisplay();
     }
     
-    public ClockDisplay(int hour, int minute, int second){
+    public ClockDisplay(int hour, int minute, int second){ // Sama, tapi ada inputan di BlueJ
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
         seconds = new NumberDisplay(60);
         setTime(hour, minute, second);
     }
     
-    public void timeTick(){
+    public void timeTick(){ // Untuk meng-increment waktu, menggunakan fungsi "increment" di NumberDisplay
         seconds.increment();
         if(seconds.getValue() == 0){
             minutes.increment();
@@ -30,19 +30,18 @@ public class ClockDisplay{
     }
 
     
-    public void setTime(int hour, int minute, int second)
-    {
+    public void setTime(int hour, int minute, int second){ // untuk mengeset waktunya
         hours.setValue(hour);
         minutes.setValue(minute);
         seconds.setValue(second);
         updateDisplay();
     }
    
-    public String getTime(){
+    public String getTime(){ // mengembalikan waktu dalam format hh:mm:ss
         return displayString;
     }
     
-    private void updateDisplay(){
+    private void updateDisplay(){ // untuk meng-update waktu dan direturn dalam tipe data string
         displayString = hours.getDisplayValue() + ":" +
         minutes.getDisplayValue()  + ":" +
         seconds.getDisplayValue();
