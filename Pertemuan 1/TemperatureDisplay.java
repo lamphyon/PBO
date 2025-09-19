32530e4d6c5e40ecf68b53fde25b0d5f73de022e
Abdullah Sultan Barizy <abdullahsultanbarizy@gmail.com>
2025-09-19 18:36:05 +0700

    Update TemperatureDisplay.java

--- Pertemuan 1/TemperatureDisplay.java
+++ Pertemuan 1/TemperatureDisplay.java
@@ -1,9 +1,9 @@
 import java.util.Random;
 
 public class TemperatureDisplay {
-    private Random random = new Random();
+    private Random random = new Random(); // mendapatkan angka random
 
-    public String getTemperature() {
+    public String getTemperature() { // mereturn angka random dari 25 hingga 38
         return 25 + random.nextInt(13) + "°C";
     }
 }
