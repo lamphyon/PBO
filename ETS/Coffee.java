342877474e29d076a019ec2420a3e569625cf130
Abdullah Sultan Barizy <abdullahsultanbarizy@gmail.com>
2025-10-14 11:20:49 +0700

    Update Coffee.java

--- ETS/Coffee.java
+++ ETS/Coffee.java
@@ -1,17 +1,17 @@
 public class Coffee{
-    private String name;
-    private int basePrice;
+    private String name; // atribut
+    private int basePrice; // atribut
 
-    public Coffee(String name, int basePrice){
+    public Coffee(String name, int basePrice){ // untuk menyocokkan
         this.name = name;
         this.basePrice = basePrice;
     }
 
-    public String getName(){
+    public String getName(){ // untuk input nama
         return name;
     }
 
-    public int getBasePrice(){
+    public int getBasePrice(){ // untuk input harga awal sebelum dikali size
         return basePrice;
     }
 }
